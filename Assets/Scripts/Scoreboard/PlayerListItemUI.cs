d4d1189217b41dda6ed2568dbedb2111210b3095
DevNatrix <natrixfn@gmail.com>
2023-07-09 11:33:56 +0200

    Steam Name In Scoreboard

---
+++ Assets/Scripts/Scoreboard/PlayerListItemUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerListItemUI : MonoBehaviour
+{
+
+    public TextMeshProUGUI PlayerName;
+    public RawImage PlayerIcon;
+}
