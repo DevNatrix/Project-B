f1d96db5125523dc9975138a15ea070bdf48b9ae
DevNatrix <natrixfn@gmail.com>
2023-07-10 16:39:51 +0200

    buysystem update

---
+++ Assets/Scripts/BuySystem/BuySystem.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BuySystem : MonoBehaviour
+{
+    PlayerControls playerControls;
+    public GameObject BuyScreenGO;
+    public GameObject WeaponHolder;
+    bool BuyScreenVisual = false;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    private void Update()
+    {
+        BuyScreen();
+    }
+
+    void BuyScreen()
+    {
+        if(playerControls.Weapon.BuyScreen.WasPressedThisFrame() && !BuyScreenVisual)
+        {
+            BuyScreenGO.SetActive(true);
+            BuyScreenVisual = true;
+        }
+        else if(playerControls.Weapon.BuyScreen.WasPressedThisFrame() && BuyScreenVisual)
+        {
+            BuyScreenGO.SetActive(false);
+            BuyScreenVisual = false;
+        }
+    }
+
+    public void BuyWeapon(GameObject WeaponType)
+    {
+        Instantiate(WeaponType, WeaponHolder.transform);
+        Debug.Log($"Bought {WeaponType}");
+    }
+}
--- Assets/Scripts/WeaponSystem/WeaponSystem.cs
+++ Assets/Scripts/WeaponSystem/WeaponSystem.cs
@@ -1,52 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class WeaponSystem : MonoBehaviour
 {
-    PlayerControls playerControls;
-    public GameObject BuyScreenGO;
-    public GameObject WeaponHolder;
-    bool BuyScreenVisual = false;
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        playerControls = new PlayerControls();
+        
     }
 
-    private void OnEnable()
+    // Update is called once per frame
+    void Update()
     {
-        playerControls.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerControls.Disable();
-    }
-
-    private void Update()
-    {
-        BuyScreen();
-    }
-
-    void BuyScreen()
-    {
-        if(playerControls.Weapon.BuyScreen.WasPressedThisFrame() && !BuyScreenVisual)
-        {
-            BuyScreenGO.SetActive(true);
-            BuyScreenVisual = true;
-        }
-        else if(playerControls.Weapon.BuyScreen.WasPressedThisFrame() && BuyScreenVisual)
-        {
-            BuyScreenGO.SetActive(false);
-            BuyScreenVisual = false;
-        }
-    }
-
-    public void BuyWeapon(GameObject WeaponType)
-    {
-        Instantiate(WeaponType, WeaponHolder.transform);
-        Debug.Log($"Bought {WeaponType}");
+        
     }
 }
