00612fa0c11cf934a7dc964c6de06d183eeff39b
TheRad1 <18326@verona.k12.wi.us>
2023-07-31 16:17:16 -0500

    added main menu

---
+++ Assets/Scripts/GUI/MenuController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+	PlayerControls playerControls;
+	public GameObject startMenu;
+	public GameObject menuParent;
+	public List<GameObject> subMenus;
+	bool menu = false;
+
+	private void Awake()
+	{
+		playerControls = new PlayerControls();
+
+		playerControls.UI.toggleMenu.performed += OnKeyPerformed;
+	}
+
+	private void OnEnable()
+	{
+		playerControls.Enable();
+	}
+
+	private void OnDisable()
+	{
+		playerControls.Disable();
+	}
+
+	private void OnKeyPerformed(InputAction.CallbackContext context)
+	{
+		toggleMenu();
+	}
+
+	public void toggleMenu()
+	{
+		menu = !menu;
+		if (menu)
+		{
+			Cursor.lockState = CursorLockMode.None;
+			menuParent.SetActive(true);
+			foreach (GameObject subMenu in subMenus)
+			{
+				subMenu.SetActive(false);
+			}
+			startMenu.SetActive(true);
+		}
+		else
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			menuParent.SetActive(false);
+		}
+	}
+
+	//button events
+	public void enableGameObject(GameObject objectToEnable)
+	{
+		objectToEnable.SetActive(true);
+	}
+	public void disableGameObject(GameObject objectToDisable)
+	{
+		objectToDisable.SetActive(false);
+	}
+
+	public void leaveServer()
+	{
+		SceneManager.LoadScene(0);
+	}
+}
