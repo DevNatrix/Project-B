1c307cc58c1190dbd836a2d1206f7882cb4b6cfe
TheRad1 <18326@verona.k12.wi.us>
2023-08-01 16:23:49 -0500

    small thing

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+
 
 public class Health : MonoBehaviour
 {
     public int health;
+    int maxHealth = 100;
+	public Slider slider;
 
     public void TakeDamage(int _damage)
     {
 		Debug.Log("Other client took " +  _damage + " damage");
         health -= _damage;
+		slider.value = (float)health/(float)maxHealth;
     }
 	public void SetHealth(int _health)
     {
         health = _health;
-    }
+		slider.value = (float)health / (float)maxHealth;
+	}
 }
