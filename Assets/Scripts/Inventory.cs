be9e6a213f3c7bc074b022130473dbde04552a56
Natrix <natrixfn@gmail.com>
2023-08-13 05:34:14 +0200

    inv cleanup + cube not getting shot fix

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -16,15 +16,12 @@ public class Inventory : MonoBehaviour
     [SerializeField] private GameObject defaultMelee;
     [SerializeField] private GameObject weaponSlotVisual; // Prefab for visual representation of selected weapon
 
-    private GameObject weaponSlotInstance;
-
     void Start()
     {
         InventoryVisual.SetActive(false);
         primaryWeapon = defaultPrimary;
         secondaryWeapon = defaultSecondary;
         meleeWeapon = defaultMelee;
-        CreateWeaponSlotVisual();
     }
 
     private void Awake()
@@ -51,37 +48,15 @@ public class Inventory : MonoBehaviour
     public void SelectPrimary(GameObject _PrimaryPrefab)
     {
         primaryWeapon = _PrimaryPrefab;
-        UpdateWeaponSlotVisual();
     }
 
     public void SelectSecondary(GameObject _SecondaryPrefab)
     {
         secondaryWeapon = _SecondaryPrefab;
-        UpdateWeaponSlotVisual();
     }
 
     public void SelectMelee(GameObject _MeleePrefab)
     {
         meleeWeapon = _MeleePrefab;
-        UpdateWeaponSlotVisual();
-    }
-
-    //Function for inspecting weapons
-    private void CreateWeaponSlotVisual()
-    {
-        weaponSlotInstance = Instantiate(weaponSlotVisual, Vector3.zero, Quaternion.identity);
-        weaponSlotInstance.transform.SetParent(transform); // Set the weapon slot visual as a child of the inventory
-        weaponSlotInstance.SetActive(false); // Hide the weapon slot visual by default
-    }
-
-    private void UpdateWeaponSlotVisual()
-    {
-        if (weaponSlotInstance != null)
-        {
-            GameObject selectedWeaponPrefab = primaryWeapon; // Update this based on the currently selected weapon (primary, secondary, or melee)
-            // You can set the position and rotation of the weaponSlotInstance here based on your UI layout
-            // For example, if you have a 2D UI, you might set the position relative to the screen, or if it's a 3D UI, you might set it to a specific location on the player's hand.
-            weaponSlotInstance.SetActive(true); // Show the weapon slot visual
-        }
     }
 }
