5d73cbd7dd460ac55d3d35d148e755cd8c22e117
TheRad1 <18326@verona.k12.wi.us>
2023-08-15 21:34:38 -0500

    working on making the controller able to go up stairs

---
+++ Assets/StairFix.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairFix : MonoBehaviour
+{
+	[SerializeField] float upIncrement;
+	[SerializeField] Rigidbody playerRigidbody;
+
+	public void OnTriggerStay(Collider other)
+	{
+		
+	}
+}
